894d5875fe5b10f3a5cea7b74909062e07d27aa0
wsoftwaresm <69670953+wsoftwaresm@users.noreply.github.com>
2020-08-15 00:42:00 +0300

    Add files via upload

---
+++ sql_giris/sql_giris/sql_giris/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace sql_giris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sql_baglanti = new System.Windows.Forms.Button();
+            this.bgl_adi = new System.Windows.Forms.Label();
+            this.bgl_soyadi = new System.Windows.Forms.Label();
+            this.bgl_numarasi = new System.Windows.Forms.Label();
+            this.txt_adi = new System.Windows.Forms.TextBox();
+            this.txt_soyadi = new System.Windows.Forms.TextBox();
+            this.txt_numarasi = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_kisisil = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sql_baglanti
+            // 
+            this.sql_baglanti.BackColor = System.Drawing.Color.DarkGray;
+            this.sql_baglanti.Location = new System.Drawing.Point(26, 137);
+            this.sql_baglanti.Name = "sql_baglanti";
+            this.sql_baglanti.Size = new System.Drawing.Size(102, 38);
+            this.sql_baglanti.TabIndex = 0;
+            this.sql_baglanti.Text = "Kişi Ekle";
+            this.sql_baglanti.UseVisualStyleBackColor = false;
+            this.sql_baglanti.Click += new System.EventHandler(this.sql_baglanti_Click);
+            // 
+            // bgl_adi
+            // 
+            this.bgl_adi.AutoSize = true;
+            this.bgl_adi.Location = new System.Drawing.Point(23, 41);
+            this.bgl_adi.Name = "bgl_adi";
+            this.bgl_adi.Size = new System.Drawing.Size(29, 13);
+            this.bgl_adi.TabIndex = 1;
+            this.bgl_adi.Text = "Adı:";
+            // 
+            // bgl_soyadi
+            // 
+            this.bgl_soyadi.AutoSize = true;
+            this.bgl_soyadi.Location = new System.Drawing.Point(23, 75);
+            this.bgl_soyadi.Name = "bgl_soyadi";
+            this.bgl_soyadi.Size = new System.Drawing.Size(49, 13);
+            this.bgl_soyadi.TabIndex = 2;
+            this.bgl_soyadi.Text = "Soyadı:";
+            // 
+            // bgl_numarasi
+            // 
+            this.bgl_numarasi.AutoSize = true;
+            this.bgl_numarasi.Location = new System.Drawing.Point(23, 107);
+            this.bgl_numarasi.Name = "bgl_numarasi";
+            this.bgl_numarasi.Size = new System.Drawing.Size(63, 13);
+            this.bgl_numarasi.TabIndex = 3;
+            this.bgl_numarasi.Text = "Numarası:";
+            // 
+            // txt_adi
+            // 
+            this.txt_adi.Location = new System.Drawing.Point(133, 34);
+            this.txt_adi.Name = "txt_adi";
+            this.txt_adi.Size = new System.Drawing.Size(224, 20);
+            this.txt_adi.TabIndex = 4;
+            // 
+            // txt_soyadi
+            // 
+            this.txt_soyadi.Location = new System.Drawing.Point(133, 68);
+            this.txt_soyadi.Name = "txt_soyadi";
+            this.txt_soyadi.Size = new System.Drawing.Size(224, 20);
+            this.txt_soyadi.TabIndex = 5;
+            // 
+            // txt_numarasi
+            // 
+            this.txt_numarasi.Location = new System.Drawing.Point(133, 100);
+            this.txt_numarasi.Name = "txt_numarasi";
+            this.txt_numarasi.Size = new System.Drawing.Size(224, 20);
+            this.txt_numarasi.TabIndex = 6;
+            this.txt_numarasi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_numarasi_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 217);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(345, 150);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentDoubleClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Eklenme Tarihi(Önce Yeni)",
+            "Eklenme Tarihi(Önce Eski)",
+            "Ad (A>Z)",
+            "Ad(Z>A)"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(345, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.Text = "Listeleme Türü";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btn_kisisil
+            // 
+            this.btn_kisisil.BackColor = System.Drawing.Color.DimGray;
+            this.btn_kisisil.Location = new System.Drawing.Point(217, 137);
+            this.btn_kisisil.Name = "btn_kisisil";
+            this.btn_kisisil.Size = new System.Drawing.Size(101, 38);
+            this.btn_kisisil.TabIndex = 9;
+            this.btn_kisisil.Text = "Kişiyi Sil";
+            this.btn_kisisil.UseVisualStyleBackColor = false;
+            this.btn_kisisil.Click += new System.EventHandler(this.btn_kisisil_Click);
+            // 
+            // Form1
+            // 
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(379, 422);
+            this.Controls.Add(this.btn_kisisil);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txt_numarasi);
+            this.Controls.Add(this.txt_soyadi);
+            this.Controls.Add(this.txt_adi);
+            this.Controls.Add(this.bgl_numarasi);
+            this.Controls.Add(this.bgl_soyadi);
+            this.Controls.Add(this.bgl_adi);
+            this.Controls.Add(this.sql_baglanti);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rehber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sql_baglanti;
+        private System.Windows.Forms.Label bgl_adi;
+        private System.Windows.Forms.Label bgl_soyadi;
+        private System.Windows.Forms.Label bgl_numarasi;
+        private System.Windows.Forms.TextBox txt_adi;
+        private System.Windows.Forms.TextBox txt_soyadi;
+        private System.Windows.Forms.TextBox txt_numarasi;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btn_kisisil;
+    }
+}
+
---
+++ sql_giris/sql_giris/sql_giris/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace sql_giris
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection baglanti = new OleDbConnection("Provider=Microsoft.ACE.Oledb.12.0; Data Source=rehber.accdb");
+        OleDbCommand komut = new OleDbCommand();
+        OleDbDataReader okuyucu;
+
+        OleDbDataAdapter da;
+        DataTable dt = new DataTable();
+
+        int id;
+        int listelemeturu = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sql_baglanti_Click(object sender, EventArgs e)
+        {
+            if (baglanti.State == ConnectionState.Open)
+            {
+                baglanti.Close();
+            }
+
+            if (sql_baglanti.Text == "Düzenlemeyi Kaydet")
+            {
+                try
+                {
+                    baglanti.Open();
+                    komut.Connection = baglanti;
+                    komut.CommandText = "UPDATE kisiler set ad='" + txt_adi.Text + "', soyad='" + txt_soyadi.Text + "', numara='" + txt_numarasi.Text + "' where id= " + id;
+                    komut.ExecuteNonQuery();
+                    MessageBox.Show("Kişi Başarıyla Güncellendi!");
+                    baglanti.Close();
+                    listele();
+                }
+                catch (Exception)
+                {
+
+
+                }
+
+
+                txt_adi.Text = "";
+                txt_soyadi.Text = "";
+                txt_numarasi.Text = "";
+                id = 0;
+                sql_baglanti.Text = "Kişi Ekle";
+            }
+            else
+            {
+
+                baglanti.Open();
+                komut.Connection = baglanti;
+                komut.CommandText = "Select * FROM kisiler WHERE numara=" + txt_numarasi.Text;
+                okuyucu = komut.ExecuteReader();//okuyucuya üstteki numara değerini atadık
+                okuyucu.Read();
+                if (okuyucu.HasRows)//aynı numara kayıtlı mı , kullanıcı adı yapılabilir
+                {
+                    MessageBox.Show("Bu numara zaten kayıtlı!");
+                }
+                else
+                {
+                    komut.Dispose();
+                    okuyucu.Dispose();
+                    komut.Connection = baglanti;
+                    komut.CommandText = "INSERT INTO kisiler (ad, soyad, numara) VALUES('" + txt_adi.Text + "','" + txt_soyadi.Text + "','" + txt_numarasi.Text + "') ";
+                    komut.ExecuteNonQuery();
+                    MessageBox.Show("Kişi Başarıyla Eklendi!");
+                    listele();
+
+                    txt_adi.Text = "";
+                    txt_soyadi.Text = "";
+                    txt_numarasi.Text = "";
+                }
+
+                baglanti.Close();
+            }
+        }
+
+        void listele()
+        {
+            if (baglanti.State == ConnectionState.Open)
+            {
+                baglanti.Close();
+            }
+            dt.Clear();//data grid temizleme-üstüne yeniden yazmasın diye
+            try
+            {
+                baglanti.Open();
+                komut.Connection = baglanti;
+
+                //listeleme türü comboboxtan gelen degere göre olacak
+                switch (listelemeturu)
+                {
+                    case 1:
+                        komut.CommandText = "SELECT * from kisiler ORDER BY id DESC";
+                        break;
+                    case 2:
+                        komut.CommandText = "SELECT * from kisiler ORDER BY id ASC";
+                        break;
+                    case 3:
+                        komut.CommandText = "SELECT * from kisiler ORDER BY ad ASC";
+                        break;
+                    case 4:
+                        komut.CommandText = "SELECT * from kisiler ORDER BY ad DESC";
+                        break;
+                    default:
+                        komut.CommandText = "SELECT * from kisiler";
+                        break;
+                }
+                
+
+
+                da = new OleDbDataAdapter(komut);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message.ToString());
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (baglanti.State == ConnectionState.Open)
+            {
+                baglanti.Close();
+            }
+            listele();
+            dataGridView1.Columns[0].HeaderText = "ID";
+            dataGridView1.Columns[1].HeaderText = "ADI";
+            dataGridView1.Columns[2].HeaderText = "SOYADI";
+            dataGridView1.Columns[3].HeaderText = "NUMARA";
+
+            dataGridView1.Columns[0].Width = 30;
+            dataGridView1.Columns[1].Width = 90;
+            dataGridView1.Columns[2].Width = 90;
+            dataGridView1.Columns[3].Width = 90;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //0 indexte yani id kolonunu alıyoruz
+            id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            txt_adi.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txt_soyadi.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txt_numarasi.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            sql_baglanti.Text = "Düzenlemeyi Kaydet";
+        }
+
+        void silme()
+        {
+
+            DialogResult dr = MessageBox.Show("kişiyi silmek istediginizden emin misiniz ?", "kişi silme işlemi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+
+                if (baglanti.State == ConnectionState.Open)
+                {
+                    baglanti.Close();
+                }
+
+                try
+                {
+                    if (id > 0)
+                    {
+                        baglanti.Open();
+                        komut.Connection = baglanti;
+                        komut.CommandText = "Delete from kisiler where id = " + id;
+                        komut.ExecuteNonQuery();
+                        MessageBox.Show("Kişi başarıyla silindi!");
+                        listele();
+                    }
+
+                }
+                catch (Exception hata)
+                {
+                    MessageBox.Show(hata.Message.ToString());
+                }
+                finally
+                {
+                    baglanti.Close();
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                listelemeturu = 1;
+            } else if (comboBox1.SelectedIndex == 1)
+            {
+                listelemeturu = 2;
+            } else if (comboBox1.SelectedIndex == 2)
+            {
+                listelemeturu = 3;
+            }else if(comboBox1.SelectedIndex == 3)
+            {
+                listelemeturu = 4;
+            }
+            listele();
+        }
+
+        private void txt_numarasi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void btn_kisisil_Click(object sender, EventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.CurrentRow.Cells[0].Value);
+            silme();
+        }
+    }
+}
